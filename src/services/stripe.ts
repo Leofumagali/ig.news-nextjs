2d5a573a2510932d7934e794b997a6b7c5c80d4d
Leo Fumagali <53956140+Leofumagali@users.noreply.github.com>
2024-12-02 12:31:49 -0300

    Update stripe.ts

--- src/services/stripe.ts
+++ src/services/stripe.ts
@@ -3,7 +3,7 @@ import Stripe from 'stripe'
 export const stripe = new Stripe(
   process.env.STRIPE_API_KEY,
   {
-    apiVersion: '2020-08-27',
+    apiVersion: '2022-08-01',
     appInfo: {
       name: 'Ignews',
       version: '0.1.0'
